7f7c503b1017153d0d5940810e45b95ad5421c3e
Geraldo Ignacio de Macedo <119904806+grdlondon@users.noreply.github.com>
2023-03-09 02:50:41 -0300

    Novas funções
    Foi colocada novas funções no formulário colocamos o botão de limpar

---
+++ index.js
@@ -0,0 +1,13 @@
+var nome = document.getElementById('digite seu nome');
+var email = document.getElementById('digite seu email');
+var password = document.getElementById('digite sua senha');
+var confirmar = document.getElementById('confirmar');
+function limpar(){
+    const limpar = document.querySelector('#limpar');
+    limpar.value= '';
+};
+    
+
+
+   
+ 
